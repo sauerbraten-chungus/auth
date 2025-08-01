3eb98a7ee305a3722ecbfd913965a79398923ab8
echo4eva <perilecho@gmail.com>
2025-07-31 21:55:45 -0700

    generate

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,30 @@
-use std::{collections::HashSet, env};
+use std::{
+    collections::HashSet,
+    env,
+    time::{SystemTime, UNIX_EPOCH},
+};
 
-use axum::{Router, extract::State, http::HeaderMap, routing::get};
+use axum::{
+    Router,
+    extract::State,
+    http::{HeaderMap, StatusCode},
+    response::IntoResponse,
+    routing::get,
+};
+use jsonwebtoken::{Algorithm, EncodingKey, Header, encode};
+use serde::Serialize;
 
 #[derive(Clone)]
 struct AppState {
     chungus_keys: HashSet<String>,
     secret_chungus: String,
+}
+
+#[derive(Serialize)]
+struct Claims {
+    exp: usize,
+    iat: usize,
+    sub: String,
 }
 
 #[tokio::main]
@@ -29,23 +48,46 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn get_jwt(headers: HeaderMap, State(state): State<AppState>) {
+async fn get_jwt(headers: HeaderMap, State(state): State<AppState>) -> impl IntoResponse {
     let api_key = headers
         .get("CHUNGUS-KEY")
         .and_then(|header_value| header_value.to_str().ok());
 
-    if let Some(key) = api_key {
-        if state.chungus_keys.contains(key) {
-            // return JWT
-        } else {
-            // return epic fail
+    match api_key {
+        Some(key) if state.chungus_keys.contains(key) => {
+            let now = SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap()
+                .as_secs() as usize;
+
+            let claims = Claims {
+                exp: now + 3600,
+                iat: now,
+                sub: "kappapenis".to_string(),
+            };
+
+            match generate_jwt(&state.secret_chungus, &claims) {
+                Ok(token) => (StatusCode::OK, token),
+                Err(_) => (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    "Failed to generate token".to_string(),
+                ),
+            }
         }
-    } else {
-        // return epic fail
+        Some(_) => (StatusCode::UNAUTHORIZED, "Invalid API Key".to_string()),
+        None => (
+            StatusCode::BAD_REQUEST,
+            "Missing chungus header".to_string(),
+        ),
     }
 }
 
-fn generate_jwt() {}
+fn generate_jwt(secret: &str, claims: &Claims) -> Result<String, jsonwebtoken::errors::Error> {
+    let header = Header::new(Algorithm::HS256);
+    let encoding_key = EncodingKey::from_secret(secret.as_ref());
+
+    encode(&header, &claims, &encoding_key)
+}
 
 fn load_api_keys() -> HashSet<String> {
     let mut api_keys = HashSet::new();
